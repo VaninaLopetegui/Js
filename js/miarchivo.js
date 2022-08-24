1140d6cf7ffafd4bcd68d120eeedd5f0216b33fc
Nina <vaninalopetegui@gmail.com>
2022-08-24 17:31:40 -0300

    first commit

---
+++ js/miarchivo.js
@@ -0,0 +1,18 @@
+let cuentoInicio = "Erase una vez";
+alert ("Hagamos un cuento juntos! 🙌");
+let cuentoUsuario1 = prompt 
+    ("Escribe el principio del cuento brevemente comenzando con "+cuentoInicio+"👇");
+if(!cuentoUsuario1.startsWith("Erase una vez")){
+    alert (`¡Ups! El principio del cuento debe ir si o si con ${cuentoInicio} al principio! vuelve a intentarlo.`);
+} else {
+    while (cuentoUsuario1.startsWith(cuentoInicio)) {
+        cuentoUsuario2 = prompt ("Muy bien! Ahora como proseguira? Continua el cuento con su desarrollo/conflicto. Tu cuento comienza asi: "+cuentoUsuario1);
+        cuentoUsuario3 = prompt ("¡Wow!🤯 que interesante, escribe su desenlace para culminar el cuento: "+(cuentoUsuario1+" "+cuentoUsuario2));
+        alert ("Excelente trabajo!🤩 el cuento realizado quedo asi: "+"✨"+(cuentoUsuario1+" "+cuentoUsuario2+","+cuentoUsuario3)+"✨")
+    }
+}
+
+
+
+
+
